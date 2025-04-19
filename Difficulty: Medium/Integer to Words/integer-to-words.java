b926ddd49cb6ee8d9878d21de6d7fc0fd2d6fd2d
farheen25 <32981759+farheen25@users.noreply.github.com>
2025-04-19 16:53:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Integer to Words/integer-to-words.java
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+
+            String ans = new Solution().convertToWords(n);
+            System.out.println("\"" + ans + "\"");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    String convertToWords(int n) {
+        // code here
+        if (n == 0) 
+            return "Zero";
+        
+        // Words for numbers 0 to 19
+        String[] units = {
+            "",        "One",       "Two",      "Three",
+            "Four",    "Five",      "Six",      "Seven",
+            "Eight",   "Nine",      "Ten",      "Eleven",
+            "Twelve",  "Thirteen",  "Fourteen", "Fifteen",
+            "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+        };
+        
+        // Words for numbers multiple of 10        
+        String[] tens = { 
+            "",     "",     "Twenty",  "Thirty", "Forty",
+            "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" 
+        };
+        
+        String[] multiplier = 
+                    {"", "Thousand", "Million", "Billion"};
+      
+        String res = "";
+        int group = 0;
+        
+        // Process number in group of 1000s
+        while (n > 0) {
+            if (n % 1000 != 0) {
+                
+                int value = n % 1000;
+                String temp = "";
+                
+                // Handle 3 digit number
+                if (value >= 100) {
+                    temp = units[value / 100] + " Hundred ";
+                    value %= 100;
+                }
+
+                // Handle 2 digit number
+                if (value >= 20) {
+                    temp += tens[value / 10] + " ";
+                    value %= 10;
+                }
+
+                // Handle unit number
+                if (value > 0) {
+                    temp += units[value] + " ";
+                }
+
+                // Add the multiplier according to the group
+                temp += multiplier[group] + " ";
+                
+                // Add this group result to overall result
+                res = temp + res;
+            }
+            n /= 1000;
+            group++;
+        }
+        
+        // Remove trailing space
+        return res.trim();
+    }
+}
