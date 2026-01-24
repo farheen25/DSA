11472c4088d86d4f8cb408b17f4391b49a8193df
farheen25 <32981759+farheen25@users.noreply.github.com>
2026-01-25 02:59:31 +0530

    Time: 52 ms (5.32%) | Memory: 56.5 MB (5.42%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,10 +1,13 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        return new ArrayList<> (Arrays.stream(strs).collect(Collectors.groupingBy(word-> Arrays.stream(word.split(""))
-        .map(s->s.toLowerCase())
-        .sorted()
-        .collect(Collectors.joining())
-        ))
-        .values());
+        return new ArrayList<> (Arrays.stream(strs).collect(
+            Collectors.groupingBy(
+                word -> Arrays.stream(word.split(""))
+                .map(c -> c.toLowerCase())
+                .sorted()
+                .collect(Collectors.joining())
+            )
+        ).values()
+        );
 }
 }
