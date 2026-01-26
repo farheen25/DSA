23d5c2df73524182ba4685624d25a1dbae04d67c
farheen25 <32981759+farheen25@users.noreply.github.com>
2026-01-26 22:52:58 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (21.84%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -16,12 +16,12 @@ class Solution {
                     ans = mid;
                     right = mid-1;
                 }
-                else if(nums[mid]< target)
+                else if(target<nums[mid])
                 {
-                    left = mid+1;
+                    right = mid-1;
                 }
                 else{
-                    right = mid-1;
+                    left = mid+1;
                 }
             }
             return ans;
@@ -38,12 +38,12 @@ class Solution {
                     ans = mid;
                     left = mid+1;
                 }
-                else if(nums[mid]< target)
+                else if(target<nums[mid])
                 {
-                    left = mid+1;
+                    right = mid-1;
                 }
                 else{
-                    right = mid-1;
+                    left = mid+1;
                 }
             }
             return ans;
